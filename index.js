1628affcd386ad44c21ae1b83e93913ea8ed16b1
regou <admin@regou.me>
2016-11-19 20:35:27 +0800

    no operator example

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by wangxiao on 19/11/2016.
+ */
+
+'use strict';
+//No operator example;
+const Rx = require('rxjs/Rx');
+
+var result$ = new Rx.Observable(function (observer) {
+	let timmer = setInterval(function () {
+		observer.next(Math.random());
+	},200);
+	//Throw error
+	//observer.error(err);
+
+	//self complete
+	//observer.complete();
+
+	return function(){
+		console.log('do some clean up');
+		clearInterval(timmer);
+	}
+});
+
+var observer = {
+	next: x => console.log(x),
+	error: err => console.error(err),
+	complete: done => console.info('done')
+};
+var subscription = result$.subscribe(observer);
+
+setTimeout(function () {
+	subscription.unsubscribe();
+},4*1000);
+
