040b59c66ab2c411d35087436d672b73c02154b9
regou <admin@regou.me>
2016-11-26 20:42:39 +0800

    ajax

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 | --- | --- |
 | **Custom Observable** | [`index.js`](https://github.com/regou/rx-teach/blob/master/index.js) |
 | **Basic Opearator** | [`basic-op.js`](https://github.com/regou/rx-teach/blob/master/basic-op.js) |
+| **Ajax ensure** | [`ajax.js`](https://github.com/regou/rx-teach/blob/master/ajax.js) |
---
+++ ajax.js
@@ -0,0 +1,23 @@
+'use strict';
+//Ajax operator example;
+const Rx = require('rxjs/Rx');
+const axios = require('axios');
+
+
+var users$ = Rx.Observable.from(['BBB','PixelsCommander','regou']);
+
+var userInfo$ = users$
+	.map(user => `https://api.github.com/users/${user}`)
+	.do((url)=>console.log(url))
+	.switchMap((url)=>Rx.Observable.fromPromise( axios.get(url) ));
+
+
+
+userInfo$.subscribe(function (info) {
+	console.log(info.data);
+},function (err) {
+	console.error(err);
+});
+
+
+//Home work: 学习Cancel Token，并优化此例子
