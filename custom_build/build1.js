c9c232ad53b7c546f1c77ecbabefdf5ef206a21a
regou <admin@regou.me>
2016-12-04 21:15:41 +0800

    build

--- custom_build/build1.js
+++ custom_build/build1.js
@@ -4,7 +4,7 @@ function nextCb(val) {
 	console.log('next:' + val);
 }
 
-function errorCb() {
+function errorCb(err) {
 
 }
 
@@ -15,7 +15,7 @@ function completeCb() {
 
 function ObservableFromArray(next,error,complete){
 	const arr = [1,2,3,4,5];
-	arr.forEach(next);
+	arr.forEach(num => next(num));
 	complete();
 }
 
